1d40b057d57b3225ef697f9c6091cf4c575a89bf
bagusperdanay7 <baguspyus@gmail.com>
2023-10-10 19:46:56 +0700

    fix: fixing google login

--- ks-web-app/app/Http/Controllers/GoogleLoginController.php
+++ ks-web-app/app/Http/Controllers/GoogleLoginController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Auth\Events\Registered;
 
 class GoogleLoginController extends Controller
 {
@@ -16,19 +17,22 @@ class GoogleLoginController extends Controller
     public function handlerCallback() {
         $googleUser = Socialite::driver('google')->user();
 
-        dd($googleUser);
+        // dd($googleUser);
 
         $user = User::updateOrCreate([
             'google_id' => $googleUser->id,
         ], [
             'name' => $googleUser->name,
             'email' => $googleUser->email,
+            'username' => $googleUser->name,
             'profile_picture' => $googleUser->avatar,
+            'password' => ''
             // 'github_token' => $googleUser->token,
             // 'github_refresh_token' => $googleUser->refreshToken,
         ]);
 
         Auth::login($user);
+
 
         return redirect('/');
     }
